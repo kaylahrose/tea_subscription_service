8914f59151aabdd3b5caeab164c3f0a8291912f9
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-18 16:36:40 -0600

    Create model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   has_many :teas, through: :subscriptions
+
+  validates_presence_of :first_name, :last_name, :email, :street_address
+  validates_uniqueness_of :email
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,6 @@
 class Tea < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   has_many :customers, through: :subscriptions
+
+  validates_presence_of :title, :temperature, :brew_time, :description
 end
