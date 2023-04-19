ed08316b45bded1908aceb3c59e98c8687d39515
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-18 21:14:39 -0600

    Reformat

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -17,65 +17,65 @@ describe 'subscriptions API' do
         expect(response_body[:data][:type]).to be_a String
         expect(response_body[:data][:attributes]).to be_a Hash
       end
-      
-      describe 'sad path' do
-        it 'responds with error for no/invalid customer' do
-          customer = create(:customer)
-          tea = create(:tea)
-          headers = { "CONTENT_TYPE" => "application/json" }
+    end
+    
+    describe 'sad path' do
+      it 'responds with error for no/invalid customer' do
+        customer = create(:customer)
+        tea = create(:tea)
+        headers = { "CONTENT_TYPE" => "application/json" }
 
-          post "/api/v1/customers/#{Customer.last.id+1}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
-          response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{Customer.last.id+1}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-          expect(response).to_not be_successful
-          expect(response.status).to eq(400)
-          expect(response_body[:error].first[:title]).to eq("Validation failed: Customer must exist")
-          expect(response_body[:error].first[:status]).to eq('400')
-        end
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response_body[:error].first[:title]).to eq("Validation failed: Customer must exist")
+        expect(response_body[:error].first[:status]).to eq('400')
+      end
 
-        it 'responds with error for no tea' do 
-          customer = create(:customer)
-          tea = create(:tea)
-          headers = { "CONTENT_TYPE" => "application/json" }
+      it 'responds with error for no tea' do 
+        customer = create(:customer)
+        tea = create(:tea)
+        headers = { "CONTENT_TYPE" => "application/json" }
 
-          post "/api/v1/customers/#{customer.id}/teas/#{tea.id+1}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
-          response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/teas/#{tea.id+1}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-          expect(response).to_not be_successful
-          expect(response.status).to eq(400)
-          expect(response_body[:error].first[:title]).to eq("Validation failed: Tea must exist")
-          expect(response_body[:error].first[:status]).to eq('400') 
-        end
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response_body[:error].first[:title]).to eq("Validation failed: Tea must exist")
+        expect(response_body[:error].first[:status]).to eq('400') 
+      end
 
-        it 'doesnt create subscription with empty params' do
-          customer = create(:customer)
-          tea = create(:tea)
-          headers = { "CONTENT_TYPE" => "application/json" }
+      it 'doesnt create subscription with empty params' do
+        customer = create(:customer)
+        tea = create(:tea)
+        headers = { "CONTENT_TYPE" => "application/json" }
 
-          post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({price: 10})
-          response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({price: 10})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-          expect(response).to_not be_successful
-          expect(response.status).to eq(400)
-          expect(response_body[:error].first[:title]).to eq("Validation failed: Frequency can't be blank")
-          expect(response_body[:error].first[:status]).to eq('400')  
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response_body[:error].first[:title]).to eq("Validation failed: Frequency can't be blank")
+        expect(response_body[:error].first[:status]).to eq('400')  
 
-          post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: 'monthly'})
-          response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: 'monthly'})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-          expect(response).to_not be_successful
-          expect(response.status).to eq(400)
-          expect(response_body[:error].first[:title]).to eq("Validation failed: Price can't be blank")
-          expect(response_body[:error].first[:status]).to eq('400')  
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response_body[:error].first[:title]).to eq("Validation failed: Price can't be blank")
+        expect(response_body[:error].first[:status]).to eq('400')  
 
-          post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({})
-          response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-          expect(response).to_not be_successful
-          expect(response.status).to eq(400)
-          expect(response_body[:error].first[:title]).to eq("Validation failed: Price can't be blank, Frequency can't be blank")
-          expect(response_body[:error].first[:status]).to eq('400')  
-        end
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        expect(response_body[:error].first[:title]).to eq("Validation failed: Price can't be blank, Frequency can't be blank")
+        expect(response_body[:error].first[:status]).to eq('400')  
       end
     end
   end
