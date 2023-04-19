75ca155c3b61f6623e372d2e2b6062e088681f3f
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-18 20:55:23 -0600

    Test: create tea subscription sad path

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
 describe 'subscriptions API' do
-  it 'create a customers tea subscription' do
-    customer = create(:customer)
-    tea = create(:tea)
-    headers = { "CONTENT_TYPE" => "application/json" }
+  describe 'POST /api/v1/customers/:id/teas/:id/subscription' do
+    describe 'happy path' do
+      it 'create a customers tea subscription' do
+        customer = create(:customer)
+        tea = create(:tea)
+        headers = { "CONTENT_TYPE" => "application/json" }
 
-    post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
-    response_body = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
+        response_body = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(201)
-    expect(response_body[:data][:id]).to be_a String
-    expect(response_body[:data][:type]).to be_a String
-    expect(response_body[:data][:attributes]).to be_a Hash
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+        expect(response_body[:data][:id]).to be_a String
+        expect(response_body[:data][:type]).to be_a String
+        expect(response_body[:data][:attributes]).to be_a Hash
+      end
+
+      describe 'sad path' do
+        it 'responds with error for no/invalid customer' do
+          tea = create(:tea)
+          headers = { "CONTENT_TYPE" => "application/json" }
+
+          post "/api/v1/customers/#{Customer.last.id+1}/teas/#{tea.id}/subscriptions", headers: headers, params: JSON.generate({frequency: "monthly", price: 10})
+          response_body = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to_not be_successful
+        end
+        it 'responds with error for no tea'
+        it 'doesnt create subscription with empty params'
+      end
+    end
   end
 
   it 'cancels a customers tea subscription' do
